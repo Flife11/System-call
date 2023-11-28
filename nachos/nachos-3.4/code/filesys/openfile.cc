d99b56753f2ef6ec94dece66ec1adf8ea7e45907
quynluvpinky <98611090+quynluvpinky@users.noreply.github.com>
2023-11-28 13:33:20 +0700

    Update openfile.cc

--- nachos/nachos-3.4/code/filesys/openfile.cc
+++ nachos/nachos-3.4/code/filesys/openfile.cc
@@ -34,6 +34,14 @@ OpenFile::OpenFile(int sector)
     seekPosition = 0;
 }
 
+OpenFile::OpenFile(int sector, int fileType)
+{
+    hdr = new FileHeader;
+    hdr->FetchFrom(sector);
+    seekPosition = 0;
+    type = fileType;
+}
+
 //----------------------------------------------------------------------
 // OpenFile::~OpenFile
 // 	Close a Nachos file, de-allocating any in-memory data structures.
