9835917ca7b8bb23e3fbfcf8effe6478c46ae4f2
21120588 <julianng1110@gmail.com>
2023-12-03 22:47:52 +0700

    Sua ham bubble sort, chia ra 1 file rieng

---
+++ nachos/nachos-3.4/code/test/bubbleSort.c
@@ -0,0 +1,67 @@
+#include "syscall.h";
+
+int compare(int a, int b, int type) {  // Type 1: Ascending. Type 2: Descending
+    if (type == 1) {
+        if (a > b) {  // Truoc > Sau thi swap
+            return 0;
+        } else
+            return 1;
+    } else {
+        if (a < b) {
+            return 0;
+        } else
+            return 1;
+    }
+}
+
+int main() {
+    int a[101]; /* size of physical memory; with code, we'll run out of space!*/
+
+    int n, type, i, j, temp;
+
+    /* first initialize the array, in reverse sorted order */
+    // Nhap so ki tu
+    do {
+        n = 0;
+        PrintString("Nhap so ki so n (n <= 100)");
+        n = ReadInt();
+    } while (n > 100 || n <= 0);
+
+    // Chon loai sap xep
+    do {
+        type = 0;
+        PrintString("Nhap loai sap xep (1: Tang dan || 2: Giam dan)");
+        type = ReadInt();
+    } while (type != 1 || type != 2);
+
+    // Input array
+    for (i = 0; i < n; ++i) {
+        PrintString("a[");
+        PrintInt(i);
+        PrintString("] = ");
+        a[i] = ReadInt();
+    }
+
+    /* then sort! */
+    // Bubble sort
+    for (i = 0; i < n - 1; ++i) {
+        for (j = 0; j < n - i - 1; ++j) {
+            if (!compare(a[j], a[j + 1], type)) {
+                temp = a[j];
+                a[i] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+
+    // Print result
+    PrintString("Mang da sap xep\n");
+    for (i = 0; i < n; i++) {
+        PrintInt(a[i]);
+        PrintChar(' ');
+    }
+    
+    
+    Halt();
+    Exit(a[0]); 
+}
--- nachos/nachos-3.4/code/userprog/exception.cc
+++ nachos/nachos-3.4/code/userprog/exception.cc
@@ -22,8 +22,8 @@
 // of liability and disclaimer of warranty provisions.
 
 #include "copyright.h"
-#include "system.h"
 #include "syscall.h"
+#include "system.h"
 
 #define MAX_BUFFER_LENGTH 255
 
@@ -33,24 +33,22 @@ Input: - User space address (int)
 Output:- Buffer (char*)
 Purpose: Copy buffer from User memory space to System memory space
 */
-char *User2System(int virtAddr, int limit)
-{
-    int i; // index
+char* User2System(int virtAddr, int limit) {
+    int i;  // index
     int oneChar;
-    char *kernelBuf = NULL;
-    kernelBuf = new char[limit + 1]; //need for terminal string
+    char* kernelBuf = NULL;
+    kernelBuf = new char[limit + 1];  // need for terminal string
     if (kernelBuf == NULL)
         return kernelBuf;
     memset(kernelBuf, 0, limit + 1);
     // for (int i = 0; i < limit+1; ++i) {
     //     kernelBuf[i] = '\0';
     // }
-    //printf("\n Filename u2s:");
-    for (i = 0; i < limit; i++)
-    {
+    // printf("\n Filename u2s:");
+    for (i = 0; i < limit; i++) {
         machine->ReadMem(virtAddr + i, 1, &oneChar);
         kernelBuf[i] = (char)oneChar;
-        //printf("%c",kernelBuf[i]);
+        // printf("%c",kernelBuf[i]);
         if (oneChar == 0)
             break;
     }
@@ -63,16 +61,14 @@ Input: - User space address (int)
 Output:- Number of bytes copied (int)
 Purpose: Copy buffer from System memory space to User memory space
 */
-int System2User(int virtAddr, int len, char *buffer)
-{
+int System2User(int virtAddr, int len, char* buffer) {
     if (len < 0)
         return -1;
     if (len == 0)
         return len;
     int i = 0;
     int oneChar = 0;
-    do
-    {
+    do {
         oneChar = (int)buffer[i];
         machine->WriteMem(virtAddr + i, 1, oneChar);
         i++;
@@ -80,16 +76,13 @@ int System2User(int virtAddr, int len, char *buffer)
     return i;
 }
 
-
 // hàm tăng giá trị thanh ghi PC
-void IncreasePC()
-{
-	int counter = machine->ReadRegister(PCReg); //lưu giá trị thanh ghi lệnh hiện tại vào counter
-   	machine->WriteRegister(PrevPCReg, counter); //lưu giá trị counter vào thanh ghi lệnh trước đó
-    	counter = machine->ReadRegister(NextPCReg); //lưu giá trị thanh ghi tiếp theo vào counter
-    	machine->WriteRegister(PCReg, counter); //lưu giá trị counter vào thanh ghi lệnh hiện hiện tại
-   	machine->WriteRegister(NextPCReg, counter + 4); //lưu giá trị counter+4 vào thanh ghi lệnh kế tiếp
-
+void IncreasePC() {
+    int counter = machine->ReadRegister(PCReg);      // lưu giá trị thanh ghi lệnh hiện tại vào counter
+    machine->WriteRegister(PrevPCReg, counter);      // lưu giá trị counter vào thanh ghi lệnh trước đó
+    counter = machine->ReadRegister(NextPCReg);      // lưu giá trị thanh ghi tiếp theo vào counter
+    machine->WriteRegister(PCReg, counter);          // lưu giá trị counter vào thanh ghi lệnh hiện hiện tại
+    machine->WriteRegister(NextPCReg, counter + 4);  // lưu giá trị counter+4 vào thanh ghi lệnh kế tiếp
 }
 //----------------------------------------------------------------------
 // ExceptionHandler
@@ -114,190 +107,214 @@ void IncreasePC()
 //	are in machine.h.
 //----------------------------------------------------------------------
 
-
-void ExceptionHandler(ExceptionType which)
-{
+void ExceptionHandler(ExceptionType which) {
     int type = machine->ReadRegister(2);
 
-    switch (which)
-    {
-    case NoException:
-        return;
-    case SyscallException:
-        switch (type)
-        {
-        case SC_Halt:
-            DEBUG('a', "\n Shutdown, initiated by user program.");
+    switch (which) {
+        case NoException:
+            return;
+        case PageFaultException:
+            DEBUG('a', "\t No translation valid found");
+            printf("\n\n No valid translation found");
+            interrupt->Halt();
+            break;
+        case ReadOnlyException:
+            DEBUG('a', "\t Write attempted to read-only page");
+            printf("\n\n Write attempted to read-only page");
             interrupt->Halt();
             break;
 
-        case SC_PrintString:
-        {
-            int Address;
-            char *buffer;
-            Address = machine->ReadRegister(4); //đọc địa chỉ bắt đầu của vùng nhớ chứa chuỗi sẽ in
-            buffer = User2System(Address, MAX_BUFFER_LENGTH); //copy chuỗi từ vùng nhớ của userspace sang kernel
-            int length;
-            length = 0;
-            while (buffer[length] != '\0') //đếm độ dài thật sự của chuỗi muốn in
-            {
-                length++;
-            }
-            gSynchConsole->Write(buffer, length + 1);  //in chuỗi ra console
-
-	    delete[] buffer; //giải phóng vùng nhớ
-            IncreasePC(); //tăng program counter
-            
+        case BusErrorException:
+            DEBUG('a', "\t Translation resulted invalid physical address");
+            printf("\n\n Translation resulted invalid physical address");
+            interrupt->Halt();
             break;
-        }
 
-	    case SC_ReadString:
-        {
-            int Address;
-	        int length;
-	        char* buffer;
-
-	        Address = machine->ReadRegister(4); //lấy địa chỉ vùng nhớ ở userspace sẽ lưu chuỗi ở console
-	        length = machine->ReadRegister(5);  //lấy độ dài tối đa của chuỗi
-
-	        buffer = User2System(Address, length); //copy chuỗi từ userspace sang kernel	    
-	        gSynchConsole->Read(buffer, length); //đọc chuỗi từ userspace với độ dài tối đa
-	        int truelength = 0;
-	        while (buffer[truelength] != '\0') //đếm độ dài thật sự của chuỗi
-                {
-                    truelength++;
-                }
-	        System2User(Address, truelength, buffer); //copy chuỗi từ vùng nhớ kernel sang userspace
-	        delete[] buffer;  //giải phóng vùng nhớ
-	        IncreasePC();  //tăng program counter
-
-	        break;
-        }
-
-	    case SC_PrintChar:
-	    {
-		    char ch = (char)machine->ReadRegister(4);
-		    gSynchConsole->Write(&ch, 1);
-		    IncreasePC();
-		    break;
-	    }
-
-	    case SC_ReadChar:
-	    {
-		    int maxBytes = 255;
-		    char* buffer = new char[255];
-		    int numBytes = gSynchConsole->Read(buffer, maxBytes);
-
-		    if(numBytes > 1) {
-			    printf("Chi duoc nhap duy nhat 1 ky tu!\n");
-			    DEBUG('a', "\nERROR: Chi duoc nhap duy nhat 1 ky tu!");
-			    machine->WriteRegister(2, 0);
-		    }
-		    else if(numBytes == 0) {
-			    printf("Ky tu rong!\n");
-			    DEBUG('a', "\nERROR: Ky tu rong!");
-			    machine->WriteRegister(2, 0);
-		    }
-		    else {
-			    char c = buffer[0];
-			    machine->WriteRegister(2, c);
-		    }
-		    delete buffer;
-		    IncreasePC();
-		
-		    break;		
-	        }
-        
-        case SC_PrintInt:
-        {
-            // đọc từ thanh ghi
-            int number = machine->ReadRegister(4); //đọc địa chỉ bắt đầu của vùng nhớ chứa chuỗi sẽ in
-            char* buffer = NULL;
-            
-            // check negative
-            bool isNegative = false;
-            if (number < 0)
-                isNegative = true;
-
-            // lấy trị tuyệt đối
-            if (isNegative == true)
-                number *= -1;
-            
-            // length
-            int length = 0;
-            int temp = number;
-            do {
-                temp /= 10;
-                length++;
-            } while (temp > 0);
-
-            // fix length
-            int head = 0;
-            if (isNegative)
-            {
-                length++; // chừa chỗ cho "-"
-                head = 1; // head[0] = "-"
-            }
-
-            // chuyển số thành chuỗi
-            buffer = new char[length + 1];
-            buffer[length] = '\0';
-            temp = number;
-            for (int i = length - 1; i >= head; i--)
-            {
-                buffer[i] = (temp % 10 + '0');
-                temp /= 10;
-            }
-
-            // fix string
-            if (isNegative)
-                buffer[0] = '-';
-
-            // in chuỗi 
-            gSynchConsole->Write(buffer, length);  
-
-            delete[] buffer; //giải phóng vùng nhớ
-            IncreasePC(); //tăng program counter
+        case AddressErrorException:
+            DEBUG('a', "\t Unaligned reference or out of the end of the address space");
+            printf("\n\n Unaligned reference or out of the end of the address space");
+            interrupt->Halt();
             break;
-        }
 
-        case SC_ReadInt:
-        {
-            char* buffer = new char[MAX_BUFFER_LENGTH + 1];
-            int length = gSynchConsole->Read(buffer, MAX_BUFFER_LENGTH);
+        case OverflowException:
+            DEBUG('a', "\t Integer overflow in add or sub.");
+            printf("\n\n Integer overflow in add or sub.");
+            interrupt->Halt();
+            break;
 
-            int number = 0;
+        case IllegalInstrException:
+            DEBUG('a', "\t Illegal instr.");
+            printf("\n\n Illegal instr.");
+            interrupt->Halt();
+            break;
 
-            int pow10 = 1;
-            // đọc từ đuôi
-            for (int i = length - 1; i >= 0; --i) {
-                if ('-' == buffer[i] && i == 0) {
-                    // number là số âm
-                    number *= -1;
+        case NumExceptionTypes:
+            DEBUG('a', "\t Number exception types");
+            printf("\n\n Number exception types");
+            interrupt->Halt();
+            break;
+        case SyscallException:
+            switch (type) {
+                case SC_Halt:
+                    DEBUG('a', "\n Shutdown, initiated by user program.");
+                    interrupt->Halt();
+                    break;
+
+                case SC_PrintString: {
+                    int Address;
+                    char* buffer;
+                    Address = machine->ReadRegister(4);                // đọc địa chỉ bắt đầu của vùng nhớ chứa chuỗi sẽ in
+                    buffer = User2System(Address, MAX_BUFFER_LENGTH);  // copy chuỗi từ vùng nhớ của userspace sang kernel
+                    int length;
+                    length = 0;
+                    while (buffer[length] != '\0')  // đếm độ dài thật sự của chuỗi muốn in
+                    {
+                        length++;
+                    }
+                    gSynchConsole->Write(buffer, length + 1);  // in chuỗi ra console
+
+                    delete[] buffer;  // giải phóng vùng nhớ
+                    IncreasePC();     // tăng program counter
+
                     break;
                 }
-                else if ('0' <= buffer[i] && buffer[i] <= '9') {
-                    // tạo number
-                    number += (pow10 * (buffer[i] - '0'));
-                    pow10 *= 10;
-                }
-                else {
-                    // number lỗi = 0
-                    number = 0;
+
+                case SC_ReadString: {
+                    int Address;
+                    int length;
+                    char* buffer;
+
+                    Address = machine->ReadRegister(4);  // lấy địa chỉ vùng nhớ ở userspace sẽ lưu chuỗi ở console
+                    length = machine->ReadRegister(5);   // lấy độ dài tối đa của chuỗi
+
+                    buffer = User2System(Address, length);  // copy chuỗi từ userspace sang kernel
+                    gSynchConsole->Read(buffer, length);    // đọc chuỗi từ userspace với độ dài tối đa
+                    int truelength = 0;
+                    while (buffer[truelength] != '\0')  // đếm độ dài thật sự của chuỗi
+                    {
+                        truelength++;
+                    }
+                    System2User(Address, truelength, buffer);  // copy chuỗi từ vùng nhớ kernel sang userspace
+                    delete[] buffer;                           // giải phóng vùng nhớ
+                    IncreasePC();                              // tăng program counter
+
                     break;
                 }
+
+                case SC_PrintChar: {
+                    char ch = (char)machine->ReadRegister(4);
+                    gSynchConsole->Write(&ch, 1);
+                    IncreasePC();
+                    break;
+                }
+
+                case SC_ReadChar: {
+                    int maxBytes = 255;
+                    char* buffer = new char[255];
+                    int numBytes = gSynchConsole->Read(buffer, maxBytes);
+
+                    if (numBytes > 1) {
+                        printf("Chi duoc nhap duy nhat 1 ky tu!\n");
+                        DEBUG('a', "\nERROR: Chi duoc nhap duy nhat 1 ky tu!");
+                        machine->WriteRegister(2, 0);
+                    } else if (numBytes == 0) {
+                        printf("Ky tu rong!\n");
+                        DEBUG('a', "\nERROR: Ky tu rong!");
+                        machine->WriteRegister(2, 0);
+                    } else {
+                        char c = buffer[0];
+                        machine->WriteRegister(2, c);
+                    }
+                    delete buffer;
+                    IncreasePC();
+
+                    break;
+                }
+
+                case SC_PrintInt: {
+                    // đọc từ thanh ghi
+                    int number = machine->ReadRegister(4);  // đọc địa chỉ bắt đầu của vùng nhớ chứa chuỗi sẽ in
+                    char* buffer = NULL;
+
+                    // check negative
+                    bool isNegative = false;
+                    if (number < 0)
+                        isNegative = true;
+
+                    // lấy trị tuyệt đối
+                    if (isNegative == true)
+                        number *= -1;
+
+                    // length
+                    int length = 0;
+                    int temp = number;
+                    do {
+                        temp /= 10;
+                        length++;
+                    } while (temp > 0);
+
+                    // fix length
+                    int head = 0;
+                    if (isNegative) {
+                        length++;  // chừa chỗ cho "-"
+                        head = 1;  // head[0] = "-"
+                    }
+
+                    // chuyển số thành chuỗi
+                    buffer = new char[length + 1];
+                    buffer[length] = '\0';
+                    temp = number;
+                    for (int i = length - 1; i >= head; i--) {
+                        buffer[i] = (temp % 10 + '0');
+                        temp /= 10;
+                    }
+
+                    // fix string
+                    if (isNegative)
+                        buffer[0] = '-';
+
+                    // in chuỗi
+                    gSynchConsole->Write(buffer, length);
+
+                    delete[] buffer;  // giải phóng vùng nhớ
+                    IncreasePC();     // tăng program counter
+                    break;
+                }
+
+                case SC_ReadInt: {
+                    char* buffer = new char[MAX_BUFFER_LENGTH + 1];
+                    int length = gSynchConsole->Read(buffer, MAX_BUFFER_LENGTH);
+
+                    int number = 0;
+
+                    int pow10 = 1;
+                    // đọc từ đuôi
+                    for (int i = length - 1; i >= 0; --i) {
+                        if ('-' == buffer[i] && i == 0) {
+                            // number là số âm
+                            number *= -1;
+                            break;
+                        } else if ('0' <= buffer[i] && buffer[i] <= '9') {
+                            // tạo number
+                            number += (pow10 * (buffer[i] - '0'));
+                            pow10 *= 10;
+                        } else {
+                            // number lỗi = 0
+                            number = 0;
+                            break;
+                        }
+                    }
+
+                    machine->WriteRegister(2, number);
+                    delete[] buffer;
+                    IncreasePC();
+                    break;
+                }
+
+                default:
+                    printf("Unexpected user mode exception %d %d\n", which, type);
+                    ASSERT(FALSE);
+                    break;
             }
-
-            machine->WriteRegister(2, number);
-            delete[] buffer;
-            IncreasePC();
-            break;
-        }
-
-        default:
-            printf("Unexpected user mode exception %d %d\n", which, type);
-            ASSERT(FALSE);
-            break;
-        }
     }
 }
